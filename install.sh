272527ab77653693c5fb3209afb65d9c76bf9611
Maverobot <quzhengrobot@gmail.com>
2019-01-05 17:09:17 +0100

    update install script

--- install.sh
+++ install.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
+# Get user confirmation in console
+confirm() {
+    while [ 1 ]; do
+        read -p "Continue (y/n)?" choice
+        case "$choice" in
+            y|Y ) echo "yes"; break;;
+            n|N ) echo "no"; break;;
+            * ) ;;
+        esac
+    done
+}
+
 # Download dotfiles from github
 if [ ! -d $(eval echo "~/.dotfiles") ]; then
     git clone https://github.com/Maverobot/dotfiles.git ~/.dotfiles
@@ -9,18 +21,46 @@ fi
 
 # Install i3wm dependencies
 echo ""
-sudo apt install i3-wm i3status i3lock xautolock suckless-tools
+sudo apt install i3-wm i3status i3lock xautolock suckless-tools arandr dunst
 
 # #---Install i3wm config---# #
 if [ ! -d $(eval echo "~/.config") ]; then
     mkdir -p ~/.config
     echo "Folder .config is created under ~/"
+else
+    echo "Folder .config already exits under ~/"
 fi
 
 if [ ! -d $(eval echo "~/.config/i3") ]; then
     ln -s ~/.dotfiles/.config/i3 ~/.config/
-    echo -e "\nThe i3 config added!" 
+    echo -e "\nThe i3 config has been added!"
 else
-    echo -e "\nThe i3 config already exists under ~/.config"
+    echo -e "\nThe i3 config already exists under ~/.config. Do you want to overwrite it?"
+    case "$(confirm)" in
+        "yes" ) rm ~/.config/i3;
+                ln -s ~/.dotfiles/.config/i3 ~/.config/;
+                echo "The existing i3 config is overwritten.";;
+        "no" ) echo "The existing i3 config is left untouched";;
+    esac
+fi
+
+# #---Install systemd config---# #
+if [ ! -d $(eval echo "~/.config/systemd") ]; then
+    ln -s ~/.dotfiles/.config/systemd ~/.config/
+    # Enable the service
+    systemctl enable --user emacs
+    systemctl start --user emacs
+    echo -e "\nThe systemd config has been added!"
+else
+    echo -e "\nThe systemd config already exists under ~/.config. Do you want to overwrite it?"
+    case "$(confirm)" in
+        "yes" ) rm ~/.config/systemd;
+                ln -s ~/.dotfiles/.config/systemd ~/.config/;
+                # Enable the service
+                systemctl enable --user emacs
+                systemctl start --user emacs
+                echo "The existing systemd config is overwritten.";;
+        "no" ) echo "The existing systemd config is left untouched";;
+    esac
 fi
 
