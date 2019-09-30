d8750160d90cc34de623cbe204cc9a587a69dfc3
Maverobot <quzhengrobot@gmail.com>
2019-09-30 21:10:06 +0200

    zathura* -> zathura

--- install.sh
+++ install.sh
@@ -82,7 +82,7 @@ sudo apt install i3-gaps
 
 # Install i3wm dependencies
 echo ""
-sudo apt install i3status i3blocks i3lock xautolock suckless-tools arandr dunst xclip mps-youtube zathura* sxiv entr feh fonts-font-awesome w3m-img python3-pip scrot byzanz udiskie fcitx-googlepinyin
+sudo apt install i3status i3blocks i3lock xautolock suckless-tools arandr dunst xclip mps-youtube zathura sxiv entr feh fonts-font-awesome w3m-img python3-pip scrot byzanz udiskie fcitx-googlepinyin
 echo ""
 pip3 install --user ranger-fm youtube-dl
 
