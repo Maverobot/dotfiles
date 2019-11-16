9962ace9bb759f7ed026b87be2b0b0b68150baa8
Maverobot <quzhengrobot@gmail.com>
2019-11-16 01:21:56 +0100

    ADD: keybinding for ctrl-j/k/h/l for option selection

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,12 @@
 fish_vi_key_bindings 2>/dev/null
 
+function fish_user_key_bindings
+    bind --preset -M insert \ck up-or-search
+    bind --preset -M insert \cj down-or-search
+    bind --preset -M insert \ch backward-char
+    bind --preset -M insert \cl forward-char
+end
+
 function fish_title
     true
 end
