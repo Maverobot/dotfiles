872f7d4a0f35a3f7896c5278df17d3dd8503797c
Maverobot <quzhengrobot@gmail.com>
2019-11-16 14:09:11 +0100

    ADD: use fisher by default

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -18,6 +18,12 @@ function fish_user_key_bindings
     bind --preset -M insert \cl forward-char
 end
 
+# Install fisher
+if not functions -q fisher
+    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+    fish -c fisher
+end
 
 # For being compatible with anti-term in spacemacs
 function fish_title
