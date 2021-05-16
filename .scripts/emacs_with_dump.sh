4d1465c5017a9f91e0b487f4b56e812c0c994b11
Zheng Qu <quzhengrobot@gmail.com>
2021-05-17 01:08:03 +0200

    Revert "feat(emacs): start emacs daemon with optional dump file loading"
    This reverts commit c7fefb8c3703831ca1c564d0cf9c8acb42432930.

--- .scripts/emacs_with_dump.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-readonly DUMP_FILE=~/.emacs.d/.cache/dumps/spacemacs.pdmp
-
-if test -f "$DUMP_FILE"; then
-    /snap/bin/emacs --dump-file "$DUMP_FILE" "$@"
-else
-    /snap/bin/emacs --force-dump "$@"
-fi
