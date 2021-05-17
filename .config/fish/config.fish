82b8b1e30241c2d0be592e7e6a0fbf83ea4781b7
Zheng Qu <quzhengrobot@gmail.com>
2021-05-17 02:13:17 +0200

    fix: script to install fisher automatically

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -86,10 +86,8 @@ end
 
 # Install fisher
 if not functions -q fisher
-    set -q XDG_CONFIG_HOME
-    or set XDG_CONFIG_HOME ~/.config
-    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
-    fish -c fisher
+    set -q XDG_CONFIG_HOME or set XDG_CONFIG_HOME ~/.config
+    curl -sL https://git.io/fisher | source
 end
 
 if test -d /opt/ros/melodic
@@ -101,3 +99,5 @@ end
 function fish_title
     true
 end
+
+set fish_greeting
